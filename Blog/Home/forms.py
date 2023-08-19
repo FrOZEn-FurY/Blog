499672b6680c0ba8a1bf0fb225433de6e9a4b7ad
FrOZEn-FurY <h.nazarieh@gmail.com>
2023-08-19 23:54:53 +0330

    Added the search form for home page

---
+++ Blog/Home/forms.py
@@ -0,0 +1,14 @@
+# Django imports
+from django import forms
+
+
+class SearchForm(forms.Form):
+    search = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Search in titles',
+            }
+        )
+    )
+
--- Blog/Home/views.py
+++ Blog/Home/views.py
@@ -1,14 +1,22 @@
 # Django imports
 from django.shortcuts import render
 from django.views import View
+from django.db.models import Q
 
 # Local imports
 from Posts.models import PostModel
+from .forms import SearchForm
 
 
 class HomeView(View):
     template_name = 'Home/Home.html'
+    form_class = SearchForm
 
     def get(self, request):
-        posts = PostModel.objects.all()
-        return render(request, self.template_name, {'posts': posts})
+        form = self.form_class
+        search = request.GET.get('search', None)
+        if search:
+            posts = PostModel.objects.filter(title__contains=search)
+        else:
+            posts = PostModel.objects.all()
+        return render(request, self.template_name, {'posts': posts, 'form': form})
